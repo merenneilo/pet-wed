eebbe39eb6cb31e0e5b9cf70d1c170f8a123835a
merenneilo <Merenneilo@yandex.ru>
2026-09-22 01:05:59 +0300

    improve UX

--- src/app/app.ts
+++ src/app/app.ts
@@ -29,6 +29,12 @@ export class App {
   })();
 
   onMerciShorClic() {
-    navigator.clipboard.writeText('Мэрцишор');
+    try {
+      navigator.clipboard.writeText('Мэрцишор').then(() => {
+        alert('Кодовое слово скопировано!');
+      });
+    } catch (err) {
+      console.error("Can't write to clipboard!");
+    }
   }
 }
